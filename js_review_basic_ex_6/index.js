0ed3fcb93aaa577ce1f0c5dbf2360c0c73b8b819
AlvaroUpgrade <alvaro.ruiz@bootcamp-upgrade.com>
2022-10-11 11:02:25 +0200

    Iteration#6 Review-Basics

--- js_review_basic_ex_6/index.js
+++ js_review_basic_ex_6/index.js
@@ -6,3 +6,34 @@
 // Sugerencia de array:
 // ['Mesirve', 'Cristiano Romualdo', 'Fernando Muralla', 'Ronalguiño']
 
+const futvolPlaiers = [
+  "Mesirve",
+  "Cristiano Romualdo",
+  "Fernando Muralla",
+  "Ronalguiño",
+];
+
+var index1 = "";
+var index2 = "";
+
+function swap(array, index1, index2) {
+  if (index1 > array.length || index2 > array.length) {
+    return console.log("No hay tantas palabras en la lista");
+  }
+
+  for (let i = 0; i < array.length; i++) {
+    console.log("Palabra: " + array[i] + " Posicion: " + i);
+  }
+  let valor1 = array[index1];
+  let valor2 = array[index2];
+  //splice()
+  array.splice(index1, 1, valor2);
+  array.splice(index2, 1, valor1);
+
+  console.log("Lista Actualizada: ");
+  for (const elements of array) {
+    console.log(elements);
+  }
+}
+
+swap(futvolPlaiers, 1, 22);
