cd29a7f01360a056f95c6f7f9e1166ab44435130
AlvaroUpgrade <alvaro.ruiz@bootcamp-upgrade.com>
2022-10-11 10:11:51 +0200

    Iteration#4 Review-Basics

--- js_review_basic_ex_4/index.js
+++ js_review_basic_ex_4/index.js
@@ -1,12 +1,31 @@
 // **Iteración #4: Métodos findArrayIndex**
 
 // Crea una función llamada `findArrayIndex` que reciba como parametros un array de textos y
-// un texto y devuelve la posición del array cuando el valor del array sea igual al valor del texto que enviaste como parametro. Haz varios ejemplos y compruebalos.
+// un texto y devuelve la posición del array cuando el valor del array sea igual al valor del texto que enviaste como parametro.
+// Haz varios ejemplos y compruebalos.
 
 // Sugerencia de función:
-
 // function findArrayIndex(array, text) {}
 
 // Ej array:
-
 // ["Caracol", "Mosquito", "Salamandra", "Ajolote"];
+
+const animals = ["Caracol", "Mosquito", "Salamandra", "Ajolote"];
+
+var text = "Ajolote";
+
+function findArrayIndex(array, text) {
+  let count = 0;
+  for (const element of array) {
+    count++;
+    //contenido del array:
+    // console.log(element);
+    if (text === element) {
+      console.log("Palabra encontrada!! En la posicion: ", array.indexOf(text) + 1);
+    } else {
+      console.log("No se encuentra la palabra en la posicion: ", count);
+    }
+  }
+}
+
+findArrayIndex(animals, text);
